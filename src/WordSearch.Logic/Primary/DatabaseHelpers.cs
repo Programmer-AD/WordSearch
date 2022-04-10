d7c891a56447e7d8b6907f134ec66fad3e32c302
Andrey Dmytrenko <andrey08112003@gmail.com>
2022-04-10 20:29:26 +0300

    Optimized GetDifference

--- src/WordSearch.Logic/Primary/DatabaseHelpers.cs
+++ src/WordSearch.Logic/Primary/DatabaseHelpers.cs
@@ -9,41 +9,29 @@ namespace WordSearch.Logic.Primary
             byte result = 0;
 
             var vectorSize = Vector<sbyte>.Count;
-            var length = GetSpanLength(vectorSize, charCounts1.Length);
+            var length = charCounts1.Length;
+            var acceleratedLength = length / vectorSize * vectorSize;
+            int i = 0;
 
-            Span<byte> buffer1 = stackalloc byte[length];
-            charCounts1.CopyTo(buffer1);
-
-            Span<byte> buffer2 = stackalloc byte[length];
-            charCounts2.CopyTo(buffer2);
-
-            for (int i = 0; i < length; i += vectorSize)
+            if (acceleratedLength > 0)
             {
-                var vector1 = GetVector(buffer1, i, vectorSize);
-                var vector2 = GetVector(buffer2, i, vectorSize);
-                var absoluteDiff = (Vector<byte>)Vector.Abs(vector1 - vector2);
-                result += Vector.Dot(absoluteDiff, Vector<byte>.One);
+                var resultVector = new Vector<byte>();
+                for (; i < acceleratedLength; i += vectorSize)
+                {
+                    var vector1 = (Vector<sbyte>)new Vector<byte>(charCounts1, i);
+                    var vector2 = (Vector<sbyte>)new Vector<byte>(charCounts2, i);
+                    var absoluteDiff = (Vector<byte>)Vector.Abs(vector1 - vector2);
+                    resultVector += absoluteDiff;
+                }
+                result += Vector.Dot(resultVector, Vector<byte>.One);
+            }
+
+            for (; i < length; i++)
+            {
+                result += (byte)Math.Abs(charCounts1[i] - charCounts2[i]);
             }
 
             return result;
-        }
-
-        private static int GetSpanLength(int vectorSize, int arrayLength)
-        {
-            var length = arrayLength / vectorSize;
-            if (arrayLength % vectorSize != 0)
-            {
-                length++;
-            }
-            length *= vectorSize;
-            return length;
-        }
-
-        private static Vector<sbyte> GetVector(Span<byte> buffer, int position, int vectorSize)
-        {
-            var slice = buffer.Slice(position, vectorSize);
-            var vector = new Vector<sbyte>(slice);
-            return vector;
         }
     }
 }
