aa197ac42787b87684756a1ec9327614f65f80d8
Andrey Dmytrenko <andrey08112003@gmail.com>
2022-04-10 04:29:47 +0300

    Implement WordEncoder

--- src/WordSearch.Logic/Encoding/WordEncoder.cs
+++ src/WordSearch.Logic/Encoding/WordEncoder.cs
@@ -4,16 +4,32 @@ namespace WordSearch.Logic.Encoding
 {
     internal class WordEncoder : IWordEncoder
     {
+        private readonly IReadOnlyDictionary<char, int> charIndexes;
+
         public string Chars { get; }
 
         public WordEncoder(string chars)
         {
             Chars = chars;
+            charIndexes = GetCharIndexes();
         }
 
         public byte[] GetCharCounts(string word)
         {
-            throw new NotImplementedException();
+            var result = new byte[Chars.Length];
+            foreach (var @char in word)
+            {
+                if (charIndexes.TryGetValue(@char, out var index))
+                {
+                    result[index]++;
+                }
+            }
+            return result;
+        }
+
+        private IReadOnlyDictionary<char, int> GetCharIndexes()
+        {
+            return Chars.Select((@char, pos) => (@char, pos)).ToDictionary(x => x.@char, x => x.pos);
         }
     }
 }
